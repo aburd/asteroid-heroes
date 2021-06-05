0cf0ff4ed95304a4df05a4a5f91346780ab793cf
aburd <burdick.aaron@gmail.com>
2021-06-05 21:41:21 +0900

    add packages

--- packages/server/src/server.js
+++ packages/server/src/server.js
@@ -1,6 +1,12 @@
+const path = require('path');
 const express = require('express');
 
 const app = express();
+
+app.use('/public', express.static(path.join(__dirname, '../../client/dist')));
+app.get('/', function (req, res) {
+  res.sendFile(path.join(__dirname, '../../client/dist/index.html'));
+});
 
 app.listen(8000, function () {
   console.log('Listing at http://localhost:8000');
